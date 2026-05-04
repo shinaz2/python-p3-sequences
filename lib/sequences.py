6a20bf5d7c784e6a42c35218193d98c75c5af9b6
Shinaz <shinazupbdullahi@gmail.com>
2026-05-04 13:57:58 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    """
+    Prints the Fibonacci sequence up to the specified length.
+    
+    Args:
+        length (int): The number of Fibonacci numbers to generate and print
+    """
+    fibonacci_list = [0, 1]
+    
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        while len(fibonacci_list) < length:
+            fibonacci_list.append(fibonacci_list[-1] + fibonacci_list[-2])
+        print(fibonacci_list)
